93fba82d2d6389abc639b2dd3a10cdc2ba5f6c2c
byanofsky <byanofsky@me.com>
2017-06-19 17:41:31 -0700

    Move flask config to settings file

--- bookmarks/bookmarks.py
+++ bookmarks/bookmarks.py
@@ -1,12 +1,8 @@
 from flask import Flask
 
-app = Flask(__name__)
-app.config.from_object(__name__)
-app.config.update(dict(
-    SECRET_KEY='development key',
-    USERNAME='admin',
-    PASSWORD='default'
-))
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_object('bookmarks.default_settings')
+app.config.from_pyfile('settings.cfg', silent=True)
 
 
 @app.route('/', methods=['GET'])
