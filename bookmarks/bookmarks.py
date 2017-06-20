0f3524494ce5f4f6c65819a91d0159b6a9a09a64
byanofsky <byanofsky@me.com>
2017-06-19 17:57:41 -0700

    Add route for add_bookmark

--- bookmarks/bookmarks.py
+++ bookmarks/bookmarks.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_object('bookmarks.default_settings')
@@ -8,3 +8,10 @@ app.config.from_pyfile('settings.cfg', silent=True)
 @app.route('/', methods=['GET'])
 def front_page():
     return render_template('front_page.html')
+
+
+@app.route('/add_bookmark/', methods=['POST'])
+def add_bookmark():
+    short = request.form['short']
+    link = request.form['link']
+    return short + ' ' + link
