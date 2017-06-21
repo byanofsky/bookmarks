bd32f04f7d070c4660022d9c77109ccb8e3c92bf
byanofsky <byanofsky@me.com>
2017-06-20 20:41:40 -0700

    Extend add bookmark view
    Provide redirect on post and get to show add bookmark form

--- bookmarks/views.py
+++ bookmarks/views.py
@@ -1,14 +1,19 @@
 from bookmarks import app
-from flask import render_template, request
+from flask import flash, render_template, request, redirect, url_for
 
 
 @app.route('/', methods=['GET'])
 def front_page():
-    return render_template('front_page.html')
+    return 'home'
 
 
-@app.route('/add_bookmark/', methods=['POST'])
+@app.route('/add_bookmark/', methods=['GET', 'POST'])
 def add_bookmark():
-    short = request.form['short']
-    link = request.form['link']
-    return short + ' ' + link
+    if request.method == 'POST':
+        short = request.form['short']
+        link = request.form['link']
+        flash('Successfully added {} {}'.format(short, link),
+              category='info')
+        return redirect(url_for('front_page'), 303)
+    else:
+        return render_template('add_bookmark.html')
