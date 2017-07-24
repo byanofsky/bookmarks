af9bf340598fbe9836762393c2e1b747bb5ce365
byanofsky <byanofsky@me.com>
2017-07-24 16:55:49 -0700

    Begin setup of login, logout, register tests

--- tests/test_bookmarks.py
+++ tests/test_bookmarks.py
@@ -18,5 +18,34 @@ class FlaskrTestCase(unittest.TestCase):
         rv = self.app.get('/')
         assert b'There aren\'t any bookmarks yet.' in rv.data
 
+    def register(self, username, name, email, password):
+        return self.app.post('/register_user/', data=dict(
+            username=username,
+            name=name,
+            email=email,
+            password=password,
+            confirm=password
+        ), follow_redirects=True)
+
+    def login(self, username, password):
+        return self.app.post('/login', data=dict(
+            username=username,
+            password=password,
+            confirm=password
+        ), follow_redirects=True)
+
+    def logout(self):
+        return self.app.get('/logout', follow_redirects=True)
+
+    def test_register(self):
+        username = 'byanofsky'
+        name = 'Brandon Yanofsky'
+        email = 'byanofsky@me.com'
+        password = 'Brandon123'
+        rv = self.register(username, name, email, password)
+        # print(rv.data)
+        assert (b'Successfully registered ' in rv.data)
+
+
 if __name__ == '__main__':
     unittest.main()
