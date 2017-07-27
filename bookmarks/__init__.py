cda4167286189d4d0586e16d2a825aca5bfb9c1a
byanofsky <byanofsky@me.com>
2017-07-27 14:34:53 -0700

    Modify flask_login import
    Import only needed classes

--- bookmarks/__init__.py
+++ bookmarks/__init__.py
@@ -1,12 +1,11 @@
 from flask import Flask
 from flask_bcrypt import Bcrypt
-import flask_login
+from flask_login import LoginManager
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_object('bookmarks.default_settings')
 app.config.from_pyfile('settings.py', silent=True)
 bcrypt = Bcrypt(app)
-login_manager = flask_login.LoginManager()
-login_manager.init_app(app)
+login_manager = LoginManager(app)
 
 import bookmarks.views
